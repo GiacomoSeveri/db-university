7cafbb9f1dd9a9e763ad4bbc2451f19ccc0bcd7a
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-02-14 16:17:56 +0100

    8

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -35,7 +35,9 @@ SELECT COUNT(*)
 FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-
+SELECT * 
+FROM `teachers`
+WHERE `phone` IS NULL;
 
 
 -- ------------------------------------------ QUERY CON GROUP BY
