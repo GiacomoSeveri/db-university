22a7faa6030042de0f4f50b29ab97952f19056a0
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-02-14 16:15:57 +0100

    6

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -26,7 +26,9 @@ FROM `exams`
 WHERE `date` = DATE('2020-06-20') AND `hour` >= TIME('14:00:00');
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
-
+SELECT * 
+FROM `degrees`
+WHERE `level` LIKE 'magistrale';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
