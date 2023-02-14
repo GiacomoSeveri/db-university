d22cb3869e15d2dafc5dd88c5a0127af80da0447
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-02-14 16:27:29 +0100

    3/2

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -49,11 +49,13 @@ FROM `students`
 GROUP BY(`year`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT COUNT(*) AS 'teachers_N' , `office_address` AS 'office' 
+SELECT COUNT(*) AS 'teachers' , `office_address` AS 'office' 
 FROM `teachers` 
 GROUP BY(`office`);
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT `exam_id`, ROUND(AVG(`vote`)) AS 'vote average'
+FROM `exam_student` 
+GROUP BY(`exam_id`);
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
