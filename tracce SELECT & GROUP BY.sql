b251438d14797bcae385d69b0ce9022864750a79
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-02-14 16:20:33 +0100

    1/2

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -44,7 +44,9 @@ WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS 'new sub', YEAR(`enrolment_date`) AS `year` 
+FROM `students` 
+GROUP BY(`year`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
