e9e0ae674dd2544d040bb129a60db75975683d1e
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-02-16 08:04:23 +0100

    2/3

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -69,5 +69,14 @@ GROUP BY(`department_id`);
 
 --Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` AS 'courses name' 
-FROM `degrees` JOIN `students` ON `degrees`.`id` = `students`.`degree_id` 
+FROM `degrees` 
+JOIN `students` 
+ON `degrees`.`id` = `students`.`degree_id` 
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+--Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `degrees`.`name` AS 'courses', `departments`.`name`AS 'department' 
+FROM `departments` 
+JOIN `degrees` 
+ON `departments`.`id` = `degrees`.`department_id` 
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
